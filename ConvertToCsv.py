bacc8440ba586c130a5c68d00631694dbcb05147
Marc Ferriggi <marcferriggi@gmail.com>
2018-07-16 11:51:49 +0200

    Added example JSON Files and tested the code

--- ConvertToCsv.py
+++ ConvertToCsv.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 
 jsonPath = sys.argv[1]
+print(os.listdir(jsonPath))
 jsonUsers = sys.argv[2]
 outcsv = sys.argv[3]
 
@@ -19,19 +20,24 @@ with open(jsonUsers) as userData:
 	for data in userList:
 		userID = data["id"] #unique id given to each user by slack
 		name = data["name"] #here we are getting the username of each slack meber and not their actual name
-		user[userid] = [name]
+		user[userID] = [name]
 
-with open(outcsv) as out:
-	writer = csv.writer(out,delimiter=',',newline='\n')
+
+with open(outcsv, 'w') as out:
+	writer = csv.writer(out,delimiter=',')
+	writer.writerow(['Time Stamp','Date/Time','User Name'])
 	for channel in os.listdir(jsonPath): #for every channel
+		print(channel)
 		channel_list.append(channel)
 		with open(jsonPath + '/' + channel) as data_file:
-			data = json.load(data_file)
+			data = json.load(data_file, encoding = "latin-1")
 			for item in data:
-				if item["type"] == "message": #TO DO: Look into other possilbe types, see what we can extract
+				if item["type"] == "message":
 					#Here is where we will choose what exact data to extract from the messages
 					userFrom = user[item["user"]]
 					#timestamp
 					ts = datetime.utcfromtimestamp(float(item['ts']))
-					time = ts.strftime("%Y-%m-%d %H:%M:%S")
-					writer.writerow([time.encode('utf-8'),userFrom.encode('utf-8')])
+					date = ts.strftime("%Y-%m-%d %H:%M:%S")
+					writer.writerow([item['ts'],date,userFrom])
+#					#To add: If reactions have been made, save info on reactions
+					#To Remove: User whatever has joined a chat
